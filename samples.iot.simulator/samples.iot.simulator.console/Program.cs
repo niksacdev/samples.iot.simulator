ca6caf67883f9ec4822cc645e1d72611db8a06df
niksacdev <niksac>
2017-03-16 19:24:02 -0500

    initial project crated

---
+++ samples.iot.simulator/samples.iot.simulator.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace samples.iot.simulator.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bye World!");
+        }
+    }
+}
