879fc16b42457ca8d0e00c6f1baae92016ae1eac
Nik Sachdeva <nikhil.sachdeva@microsoft.com>
2017-03-18 01:38:33 -0500

    added license headers

--- samples.iot.core/Interfaces/IDeviceReceiveStrategy.cs
+++ samples.iot.core/Interfaces/IDeviceReceiveStrategy.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+using System;
 using System.Threading.Tasks;
 namespace samples.iot.core
 {
--- samples.iot.simulator.console/Program.cs
+++ samples.iot.simulator.console/Program.cs
@@ -1,4 +1,7 @@
-﻿//TODO: Add license and header
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
 using System;
 using System.Text;
 using System.Threading.Tasks;
